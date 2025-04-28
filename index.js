d5fb1adb3759640186912006edc7a8c47133ce06
YangJunMing08 <165028991+YangJunMing08@users.noreply.github.com>
2025-04-28 20:09:38 +0700

    Fix Bug for Send Video As Sticker
    Made function Rafael.sendVideoAsSticker

--- index.js
+++ index.js
@@ -155,6 +155,18 @@ Rafael.sendText = (jid, text, quoted = '', options) => Rafael.sendMessage(jid, {
         return trueFileName
     }
 
+Rafael.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifVid(buff, options)
+} else {
+buffer = await videoToWebp(buff)
+}
+await Rafael.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer
+}
+	
 Rafael.sendContact = async (jid, kon, quoted = '', opts = {}) => {
 	let list = []
 	for (let i of kon) {
